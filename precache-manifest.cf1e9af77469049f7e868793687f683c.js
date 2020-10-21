ad9e70524fe487bd27d97a194776935ed3cf4842
Ankit Rathore <ankitrathore@Ankits-MacBook-Air.local>
2020-10-21 12:57:54 +0530

    Updates

---
+++ precache-manifest.cf1e9af77469049f7e868793687f683c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "008ff25b74137d37217c7b80848d90c2",
+    "url": "/Netflix-clone/index.html"
+  },
+  {
+    "revision": "df22b73537cbbfff025a",
+    "url": "/Netflix-clone/static/css/main.9aaabc76.chunk.css"
+  },
+  {
+    "revision": "de91a81c080ec9e60743",
+    "url": "/Netflix-clone/static/js/2.8942851c.chunk.js"
+  },
+  {
+    "revision": "8c531855ea370064c601b9c584684a5a",
+    "url": "/Netflix-clone/static/js/2.8942851c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df22b73537cbbfff025a",
+    "url": "/Netflix-clone/static/js/main.7d8823ae.chunk.js"
+  },
+  {
+    "revision": "1f70fcb38d92599a6c08",
+    "url": "/Netflix-clone/static/js/runtime-main.d8bb3d81.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Netflix-clone/static/media/logo.5d5d9eef.svg"
+  }
+]);
